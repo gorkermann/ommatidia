e15292a985b4487536e892cd7616524245d0c3d3
graham <lolgorithm@gmail.com>
2024-03-26 23:08:47 -0700

    orbital carrier

--- boss/CarrierBoss.ts
+++ boss/CarrierBoss.ts
@@ -117,6 +117,11 @@ class Drone extends CenteredEntity {
 
 	stunned: boolean = false;
 
+	coreMaterial = new Material( 30, 1.0, 0.5 );
+
+	baseHealth = 5;
+	health = this.baseHealth;
+
 	/* property overrides */
 
 	material = new Material( 250, 1.0, 0.5 );
@@ -129,7 +134,8 @@ class Drone extends CenteredEntity {
 	anim = new Anim( {
 		//'pos': new PhysField( this, 'pos', 'vel', this.speed ), // high speed to maintain seek position
 		'alpha': new AnimField( this, 'alpha', 0.1 ),
-		'flash': new AnimField( this, 'flash', 0.1 )
+		'flash': new AnimField( this, 'flash', 0.1 ),
+		'core-skewS': new AnimField( this.coreMaterial, 'skewS', 0.1 ), 
 
 		// thread 1
 		//'angle': new PhysField( this, 'angle', 'angleVel', 0.2 ), // not pointing, so doesn't need isAngle
@@ -141,17 +147,39 @@ class Drone extends CenteredEntity {
 		this.vel = vel;
 	}
 
+	getOwnShapes(): Array<Shape> {
+		let shape = Shape.makeCircle( new Vec2( 0, 0 ), this.width, 12, 0 );
+		shape.material = this.material
+		shape.parent = this;
+
+		for ( let i = 0; i < shape.edges.length; i++ ) {
+			if ( ( i - 1 ) % 3 == 0 ) shape.edges[i].material = this.coreMaterial;
+		}
+
+		return [shape];
+	}
+
 	stun() {
 		this.savedVel = this.vel.copy();
 		this.vel.setValues( 0, 0 );
+		this.angleVel = 0;
 
 		this.stunned = true;
+
+		this.anim.pushFrame( new AnimFrame( {
+			'core-skewS': { value: -1.0 }
+		} ) );
 	}
 
 	revive() {
 		this.vel = this.savedVel.unit().times( this.speed );
 
 		this.stunned = false;
+		this.health = this.baseHealth;
+
+		this.anim.pushFrame( new AnimFrame( {
+			'core-skewS': { value: 0.0 }
+		} ) );
 	}
 
 	hitWith( otherEntity: Entity, contact: Contact ): void {
@@ -159,9 +187,15 @@ class Drone extends CenteredEntity {
 			otherEntity.removeThis = true;
 
 			if ( !this.stunned ) {
-				this.vel.scale( 0.8 );
+				// this.vel.scale( 0.8 );
 
-				if ( this.vel.length() < 1 ) {
+				// if ( this.vel.length() < 1 ) {
+				// 	this.stun();
+				// }
+				this.angleVel *= 0.8
+
+				this.health -= 1;
+				if ( this.health <= 0 ) {
 					this.stun();
 				}
 
@@ -182,17 +216,26 @@ class Drone extends CenteredEntity {
 	}
 
 	shade() {
+		let now = new Date().getTime();
+
 		this.material.skewL = this.flash;
+
+		this.coreMaterial.skewH = 15 * Math.sin( Math.PI * 2 * ( now % 1000 ) / 1000 );
 	}
+}
+
+type CarrierBossFlags = BossFlags & {
+	stunned: boolean;
 }
 
 export class CarrierBoss extends Boss {
 	flash: number = 0.0;
 
-	flags: BossFlags = {
+	flags: CarrierBossFlags = {
 		health: 0,
 		current_attack_damage: 0,
-		retreating: false
+		retreating: false,
+		stunned: false
 	};
 
 	wait: number = 0;
@@ -203,6 +246,9 @@ export class CarrierBoss extends Boss {
 
 	drones: Array<Drone> = [];
 	gutters: Array<Gutter> = [];
+
+	reviveSpeedPct: number = 0.33;
+	reviveSpeedIncrease: number = 0.03; // rad/frame
 
 	/* property overrides */
 
@@ -258,8 +304,8 @@ export class CarrierBoss extends Boss {
 		this.right.collisionMask = COL.PLAYER_BULLET;
 		this.addSub( this.right );
 
-		this.spawnDrone( 0, new Vec2( 0, 0 ) );
-		this.spawnDrone( Math.PI, new Vec2( 0, 0 ) );//Vec2.fromPolar( Math.PI + -0.5 + Math.random(), 5 ) );
+		this.spawnDrone( 100, 0, new Vec2( 0, 0 ) );
+		this.spawnDrone( 200, Math.PI, new Vec2( 0, 0 ) );//Vec2.fromPolar( Math.PI + -0.5 + Math.random(), 5 ) );
 
 		if ( spawn ) {
 			let barrier = new CarrierBossBarrier( this.pos.copy(), fieldWidth );
@@ -268,33 +314,43 @@ export class CarrierBoss extends Boss {
 		}
 	}
 
-	spawnDrone( angle: number, vel: Vec2 ) {
-		let v = Vec2.fromPolar( angle, 1 );
-		let drone = new Drone( this.pos.plus( v.times( 100 ) ), vel );
+	spawnDrone( radius: number, angle: number, vel: Vec2 ) {
+		let v = Vec2.fromPolar( 0, 1 );
+		let drone = new Drone( v.times( radius ), vel );
+		drone.angle = angle;
 
-		this.spawnEntity( drone );
+		//this.spawnEntity( drone );
 		drone.collisionGroup = COL.LEVEL;
 		drone.collisionMask = COL.LEVEL | COL.PLAYER_BULLET; // COL.LEVEL for hitting walls
-
+		this.addSub( drone );
 		this.drones.push( drone );
 
 		let gutter = new Gutter( new Vec2( 0, 0 ), wallUnit, wallUnit / 2 );
 		gutter.alpha = 0.3;
 		this.spawnEntity( gutter );
 		gutter.collisionGroup = COL.ENEMY_BULLET;
+
+		let fieldName = 'gutter-' + this.gutters.length + '-alpha';
+		this.anim.fields[fieldName] = new AnimField( gutter, 'alpha', 0.05 );
+
 		this.gutters.push( gutter );
 	}
 
 	/* Entity overrides */
 
 	hitWith( otherEntity: Entity, contact: Contact ): void {
-		if ( otherEntity instanceof Bullet ) {
-			otherEntity.removeThis = true;
+		if ( contact.sub instanceof Drone ) {
+			contact.sub.hitWith( otherEntity, contact );
 
-			if ( contact.sub != this ) return;
-			if ( this.invuln ) return;
+		} else {
+			if ( otherEntity instanceof Bullet ) {
+				otherEntity.removeThis = true;
 
-			this.damage( 1 );
+				if ( contact.sub != this ) return;
+				if ( this.invuln ) return;
+
+				this.damage( 1 );
+			}
 		}
 	}
 
@@ -313,13 +369,20 @@ export class CarrierBoss extends Boss {
 	}
 
 	reviveDrones() {
+		let healthLoss = 1 - this.getHealth() / this.maxHealth;
+		// starts at 0.05 rad/frame, increases by 0.03 rad/frame per 33% of health lost
+		let angleSpeed = ( 1 + Math.floor( healthLoss / this.reviveSpeedPct ) ) * this.reviveSpeedIncrease;
+
 		for ( let drone of this.drones ) {
 			drone.revive();
 
-			if ( drone.vel.length() == 0 ) {
-				drone.vel.set( Vec2.fromPolar( Math.PI * 2 * Math.random(), drone.speed ) );
-			}
+			drone.angleVel = angleSpeed / ( drone.pos.length() / 100 );
+			//if ( drone.vel.length() == 0 ) {
+				//drone.vel.set( Vec2.fromPolar( Math.PI * 2 * Math.random(), drone.speed ) );
+			//}
 		}
+
+		this.flags['stunned'] = false;
 	}
 
 	defaultLogic() {
@@ -329,7 +392,7 @@ export class CarrierBoss extends Boss {
 
 		/* attack change */
 
-		if ( this.anim.isDone( [0] ) ) {
+		/*if ( this.anim.isDone( [0] ) ) {
 			this.chooseAttack();
 
 			let start = Math.PI / 2;
@@ -341,42 +404,55 @@ export class CarrierBoss extends Boss {
 					'wait': { value: 0, expireOnCount: 10000 }
 				} ) );
 			}
-		}
+		}*/
 
 		/* update attack */
 
 		for ( let i = 0; i < this.drones.length; i++ ) {
-			this.gutters[i].width = ( this.drones[i].pos.minus( this.pos ) ).length();
-			this.gutters[i].pos = ( this.drones[i].pos.plus( this.pos ) ).times( 0.5 );
-			this.gutters[i].angle = ( this.drones[i].pos.minus( this.pos ) ).angle();
+			let worldPos = this.drones[i].applyTransform( new Vec2( 0, 0 ) );
 
-			if ( this.drones[i].stunned ) this.gutters[i].isGhost = true;
-			else this.gutters[i].isGhost = false;
+			this.gutters[i].width = ( worldPos.minus( this.pos ) ).length();
+			this.gutters[i].pos = ( worldPos.plus( this.pos ) ).times( 0.5 );
+			this.gutters[i].angle = ( worldPos.minus( this.pos ) ).angle();
+
+			if ( !this.flags['stunned'] && this.drones[i].stunned && this.gutters[i].alpha == 0.3 ) {
+				let frame = new AnimFrame();
+				frame.targets['gutter-' + i + '-alpha'] = new AnimTarget( 0.0 );
+				this.anim.pushFrame( frame, { threadIndex: 1 } );
+			}
 		}
 
 		let okDrones = this.drones.filter( x => !x.stunned );
 
 		// seek midpoint of drones
 		if ( okDrones.length > 0 ) {
-			this.left.angle = ( this.drones[0].pos.minus( this.pos ) ).angle();
-			this.right.angle = ( this.drones[1].pos.minus( this.pos ) ).angle();
+			this.left.angle = this.drones[0].angle;//( this.drones[0].pos.minus( this.pos ) ).angle();
+			this.right.angle = this.drones[1].angle;//( this.drones[1].pos.minus( this.pos ) ).angle();
 
-			this.anim.clear( { withTag: 'move' } );
+			// this.anim.clear( { withTag: 'move' } );
 
-			this.anim.pushFrame( new AnimFrame( {
-				'pos': { value: ( this.drones[0].pos.plus( this.drones[1].pos ) ).times( 0.5 ) }
-			} ), { threadIndex: 1, tag: 'move' } );
+			// this.anim.pushFrame( new AnimFrame( {
+			// 	'pos': { value: ( this.drones[0].pos.plus( this.drones[1].pos ) ).times( 0.5 ) }
+			// } ), { threadIndex: 1, tag: 'move' } );
 
 		// open shell
-		} else if ( okDrones.length == 0 && this.shell.alpha == 1.0 ) {
-			this.anim.clear();
+		} else if ( okDrones.length == 0 && !this.flags['stunned'] ) {
+
+			// restart drones
+			this.anim.pushFrame( new AnimFrame( {}, [
+				new FuncCall( this, 'reviveDrones', [] )
+			] ) );
+
+			// fade in gutters
+			this.anim.pushFrame( new AnimFrame( {
+				'gutter-0-alpha': { value: 0.3 },
+				'gutter-1-alpha': { value: 0.3 },
+			} ) );
 
 			// fade in shell
 			this.anim.pushFrame( new AnimFrame( {
 				'shell-alpha': { value: 1.0 }
-			}, [
-				new FuncCall( this, 'reviveDrones', [] )
-			] ) );
+			} ) );
 
 			// wait
 			this.anim.pushFrame( new AnimFrame( {
@@ -387,6 +463,8 @@ export class CarrierBoss extends Boss {
 			this.anim.pushFrame( new AnimFrame( {
 				'shell-alpha': { value: 0.0 }
 			} ) );
+
+			this.flags['stunned'] = true;
 		}
 
 	}
